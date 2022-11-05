8e74d7043af333d62c24fb0bbeb93b4cdc9b8ed2
filgueirasjulio <julio_filgueiras@live.com>
2022-11-05 18:53:51 -0300

    feat(migration): unidades
    se refere a unidades de medidas, se relaciona com product_details

---
+++ database/migrations/2022_11_05_213724_create_unities_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration { //phpcs:ignore
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unities', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        //relacionamento com product_details
+        Schema::table('product_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('unity_id');
+            $table->foreign('unity_id')->references('id')->on('unities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //remover relacionamento com product_details
+        Schema::table('product_details', function (Blueprint $table) {
+            //remove a fk
+            $table->dropForeign('product_details_unity_id_foreign');
+
+            //remove a coluna
+            $table->dropColumn('unity_id');
+        });
+
+        Schema::dropIfExists('unities');
+    }
+};
