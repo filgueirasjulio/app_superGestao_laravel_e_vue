93143aacdbe77a86776b964c0d7c82e432fb57c5
filgueirasjulio <julio_filgueiras@live.com>
2022-11-05 23:26:29 -0300

    feat(migrations): affiliates e product_affiliates

---
+++ database/migrations/2022_11_05_221215_create_product_affiliates_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration { //phpcs:ignore
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_affiliates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('affiliate_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('price')->nullable();
+            $table->integer('min_iventory')->nullable();
+            $table->integer('max_iventory')->nullable();
+            $table->timestamps();
+
+            //foreign keys
+            $table->foreign('affiliate_id')->references('id')->on('affiliates');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            //removendo colunas de products
+            Schema::table('products', function (Blueprint $table) {
+                $table->dropColumn(['price', 'min_iventory', 'max_iventory']);
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //adicionar colunas à products
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('price')->nullable();
+            $table->integer('min_iventory')->nullable();
+            $table->integer('max_iventory')->nullable();
+        });
+
+        Schema::dropIfExists('product_affiliates');
+    }
+};
